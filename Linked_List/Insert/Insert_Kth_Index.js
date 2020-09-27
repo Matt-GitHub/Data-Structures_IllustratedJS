ac21a27ab130d29d0026d6c6991e1fb26c9416ef
Matt-GitHub <matthewjamesbedard@outlook.com>
2020-09-27 13:22:18 -0400

    created 3 additional test cases for Insert_Kth_Index that covers insertion around tail and out of range

--- Linked_List/Insert/Insert_Kth_Index.js
+++ Linked_List/Insert/Insert_Kth_Index.js
@@ -22,23 +22,44 @@ class LinkedList {
       this.head.next = temp;
     }
   }
-
+  // Function accepts 2 parameters => Value and Index
+  // The value represents the value we pass into the node
+  // The index represents the position in the index we will insert the node
   insertAtKthIndex(value, index) {
+    // Create the new node value and assign it to a variable
+    // To create our node we must use our Node class
+    // Our Node class accepts 1 value => value
+    // Initialize our newNode by using => new
     let new_node = new Node(value);
-
+    // Determine if the linked list is empty
+    // If TRUE
     if (this.head === null) {
+      // Now that we know the linked list is empty we have to handle 1 edge case
+      // is the index valid?
+      // A valid index for an empty linked list is 0
       if (index !== 0) {
+        // The index is not 0
+        // This means the index is out of range
+        // Throw an error
         throw new Error('Linked List is empty');
       } else {
+        // The linked list is empty and the index is 0
+        // This is an insert head function
+        // Set the head and tail node to the new node
+        // Return
         this.head = new_node;
         this.tail = new_node;
         return;
       }
     }
-
+    // The next case we need to handle is the ability to insert at index of 0 when the linked list is not empty
+    // If the head is not null and index === 0
     if (this.head !== null && index === 0) {
+      // Store the current head as a temp variable
       let temp = this.head;
-      this.head = new_node;
+      // Reassign the head node to equal => newNode
+      this.head = newNode;
+      // Set the new head's next value => old head (temp)
       this.head.next = temp;
       return;
     }
@@ -66,11 +87,4 @@ class LinkedList {
   }
 }
 
-// let test = new LinkedList();
-// test.insertHead('value2');
-// test.insertHead('value1');
-// test.insertHead('value0');
-// test.insertAtKthIndex('this one', 3);
-// console.log('tail', test.tail);
-// console.log('before tail', test.head.next.next);
 export default LinkedList;
--- Linked_List/Insert/__tests__/Insert_Kth_Index.test.js
+++ Linked_List/Insert/__tests__/Insert_Kth_Index.test.js
@@ -40,4 +40,31 @@ describe('testing insert at Kth index functionality of linked list', () => {
     expect(LL.tail.value).toBe('thirdValue');
     expect(LL.tail.next).toBeNull();
   });
+
+  test('insert value at Index (1), which is the current tail node, tail should remain the same and previous should be the new node', () => {
+    let LL = new LinkedList();
+    LL.insertHead('lastValue');
+    LL.insertHead('firstValue');
+    LL.insertAtKthIndex('insertValue', 1);
+    expect(LL.head.value).toBe('firstValue');
+    expect(LL.head.next.value).toBe('insertValue');
+    expect(LL.tail.value).toBe('lastValue');
+    expect(LL.tail.next).toBeNull();
+  });
+
+  test('insert value at the (Index of 2) which is currently null, new node should be the new tail node and point to null', () => {
+    let LL = new LinkedList();
+    LL.insertHead('secondValue');
+    LL.insertHead('firstValue');
+    LL.insertAtKthIndex('thirdValue', 2);
+    expect(LL.head.value).toBe('firstValue');
+    expect(LL.head.next.value).toBe('secondValue');
+    expect(LL.tail.value).toBe('thirdValue');
+    expect(LL.tail.next).toBeNull();
+  });
+
+  test('Insert value at Index (100) which is out of range, should return an error', () => {
+    let LL = new LinkedList();
+    expect(() => LL.insertAtKthIndex('newValue', 100)).toThrow(Error);
+  });
 });
