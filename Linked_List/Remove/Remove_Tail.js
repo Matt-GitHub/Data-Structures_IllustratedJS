234f253c82a4987af02013b83aceb470238af275
Matt-GitHub <matthewjamesbedard@outlook.com>
2020-09-28 13:32:21 -0400

    updated test cases for remove tail and finished comments for function

--- Linked_List/Remove/Remove_Tail.js
+++ Linked_List/Remove/Remove_Tail.js
@@ -21,23 +21,37 @@ class LinkedList {
       this.tail = newNode;
     }
   }
-
+  // Remove Head function does not take any parameters
+  // It just removes the head node of a linked list
   removeTail() {
+    // If the linked list is empty
+    // There is no tail node to remove
+    // Throw a error
     if (this.head === null) {
       throw new Error('linked list is empty');
     }
-
+    // If the linked list only has one node
+    // Reset the head and tail node to equal null
     if (this.head.next === null) {
       this.head = null;
       this.tail = null;
     } else {
+      // We need to track the previous and current node
+      // The previous node will be used to set the new tail node
       let previous = null;
       let current = this.head;
+      // Iterate over the linked list until we hit the tail
+      // The current node will be the tail
+      // The previous node will be the node before the tail
       while (current.next !== null) {
         previous = current;
         current = current.next;
       }
+      // Previous node is the node before the tail
+      // Set the previous nodes next node to null
+      // Update the tail to equal the previous node
       previous.next = null;
+      this.tail = previous;
     }
   }
 }
--- Linked_List/Remove/__tests__/Remove_Tail.test.js
+++ Linked_List/Remove/__tests__/Remove_Tail.test.js
@@ -1,1 +1,41 @@
-describe();
+import LinkedList from '../Remove_Tail';
+
+describe('testing remove head functionality of linked list', () => {
+  test('initialization of linked list should result in head and tail being null', () => {
+    let LL = new LinkedList();
+    expect(LL.head && LL.tail).toBe(null);
+  });
+
+  test('removing the tail from an empty linked list should throw an error', () => {
+    let LL = new LinkedList();
+    expect(() => LL.removeTail()).toThrow(Error);
+  });
+
+  test('removing the tail from a linked list with 1 node should result in an empty linked list', () => {
+    let LL = new LinkedList();
+    LL.insertTail('firstValue');
+    LL.removeTail();
+    expect(LL.head && LL.tail).toBeNull();
+  });
+
+  test('removing the tail from a linked list with 2 nodes should result in the head node also becoming the tail node', () => {
+    let LL = new LinkedList();
+    LL.insertTail('firstValue');
+    LL.insertTail('secondValue');
+    LL.removeTail();
+    expect(LL.head.value && LL.tail.value).toBe('firstValue');
+    expect(LL.tail.next).toBeNull();
+  });
+
+  test('removing the tail from a linked list with 3 nodes should result in the middle node becoming the tail node', () => {
+    let LL = new LinkedList();
+    LL.insertTail('firstValue');
+    LL.insertTail('secondValue');
+    LL.insertTail('thirdValue');
+    LL.removeTail();
+    expect(LL.head.value).toBe('firstValue');
+    expect(LL.head.next.value).toBe('secondValue');
+    expect(LL.tail.value).toBe('secondValue');
+    expect(LL.tail.next).toBeNull();
+  });
+});
