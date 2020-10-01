11f56e93b2d5b196adfc5ee520841d2f54db7426
Matt-GitHub <matthewjamesbedard@outlook.com>
2020-10-01 02:00:18 -0400

    completed all test suits and code comments for reverse linked list

--- Linked_List/Challenges/Reverse_Linked_List.js
+++ Linked_List/Challenges/Reverse_Linked_List.js
@@ -23,14 +23,31 @@ class LinkedList {
   }
 
   reverseLinkedList() {
+    // If the linked list is empty
+    // Throw an error
+    if (this.head === null) {
+      throw new Error('linked list is empty');
+    }
+    // We need to track 2 variables: current and previous
+    // Current will track the current node
+    // Previous will track the node prior to current
     let current = this.head;
     let previous = null;
+    // Iterate over the linked list until current is equal to null
     while (current !== null) {
+      // We need to change what the current value is pointing to
+      // It is currently pointing to the next node in the linked list
+      // We need current to point to the node before it
+      // We create a variable temp to store the old next node while we reassign it
       let temp = current.next;
+      // Set the current's next node to equal the previous node
+      // Set previous to equal the current node
+      // Set current to equal the next node in the linked list (temp)
       current.next = previous;
       previous = current;
       current = temp;
     }
+    // Return the new head node
     return previous;
   }
 }
--- Linked_List/Challenges/__tests__/Reverse_Linked_List.test.js
+++ Linked_List/Challenges/__tests__/Reverse_Linked_List.test.js
@@ -3,6 +3,36 @@ import LinkedList from '../Reverse_Linked_List';
 describe('testing reverse a linked list functionality', () => {
   test('reverse an empty linked list', () => {
     let LL = new LinkedList();
-    console.log(LL.reverseLinkedList());
+    expect(() => LL.reverseLinkedList()).toThrow(Error);
+  });
+
+  test('reverse a linked list with 1 value should return the same value and a next value of null', () => {
+    let LL = new LinkedList();
+    LL.insertTail('firstValue');
+    let answer = LL.reverseLinkedList();
+    expect(answer.value).toBe('firstValue');
+    expect(answer.next).toBeNull();
+  });
+
+  test('reverse a linked list with 2 values should return the tail value and the correct next value', () => {
+    let LL = new LinkedList();
+    LL.insertTail('firstValue');
+    LL.insertTail('secondValue');
+    let answer = LL.reverseLinkedList();
+    expect(answer.value).toBe('secondValue');
+    expect(answer.next.value).toBe('firstValue');
+  });
+
+  test('reverse a linked list with 6 values should return the tail value and the correct next value', () => {
+    let LL = new LinkedList();
+    LL.insertTail('firstValue');
+    LL.insertTail('secondValue');
+    LL.insertTail('thirdValue');
+    LL.insertTail('fourthValue');
+    LL.insertTail('fifthValue');
+    LL.insertTail('sixthValue');
+    let answer = LL.reverseLinkedList();
+    expect(answer.value).toBe('sixthValue');
+    expect(answer.next.value).toBe('fifthValue');
   });
 });
