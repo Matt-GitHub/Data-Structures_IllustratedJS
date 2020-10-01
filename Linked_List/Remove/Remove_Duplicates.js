60543566fc96c7373924deb01fb5dac4bbac05ef
Matt-GitHub <matthewjamesbedard@outlook.com>
2020-10-01 15:35:13 -0400

    removed duplicates functionality

---
+++ Linked_List/Remove/Remove_Duplicates.js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  insertTail(value) {
+    let newValue = new Node(value);
+    if (this.head === null) {
+      this.head = newValue;
+      this.tail = newValue;
+    } else {
+      this.tail.next = newValue;
+      this.tail = newValue;
+    }
+  }
+
+  removeDuplicates() {
+    if (this.head === null || this.head.next === null) return head;
+    let current = this.head;
+    while (current.next !== null) {
+      if (current.value !== current.next.value) {
+        current = current.next;
+      } else {
+        current.next = current.next.next;
+      }
+    }
+    return this.head;
+  }
+}
+
+export default LinkedList;
